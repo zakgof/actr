80aabeaea5a45f477343ba1e93d35b7d79caa738
Alexander Zakusylo <zakgof@gmail.com>
2019-12-30 13:01:17 +0200

    New DedicatedThreadScheduler constructor to allow Loom's virtual threads

--- src/main/java/com/zakgof/actr/DedicatedThreadScheduler.java
+++ src/main/java/com/zakgof/actr/DedicatedThreadScheduler.java
@@ -2,29 +2,37 @@ package com.zakgof.actr;
 
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
+import java.util.concurrent.ThreadFactory;
 
+/**
+ * Scheduler that executes tasks sequentially in a single thread.
+ */
 public class DedicatedThreadScheduler implements IActorScheduler {
 
-    private ExecutorService es;
+	private ExecutorService es;
 
-    public DedicatedThreadScheduler() {
-        this.es = Executors.newSingleThreadExecutor();
-    }
+	public DedicatedThreadScheduler() {
+		this.es = Executors.newSingleThreadExecutor();
+	}
 
-    public DedicatedThreadScheduler(String name) {
-        this.es = Executors.newSingleThreadExecutor(runnable -> new Thread(runnable, "actr:" + name));
-    }
+	public DedicatedThreadScheduler(String name) {
+		this.es = Executors.newSingleThreadExecutor(runnable -> new Thread(runnable, "actr:" + name));
+	}
 
-    @Override
-    public void schedule(Runnable task, Object actorId) {
-        if (!es.isShutdown()) {
-            es.execute(task);
-        }
-    }
+	public DedicatedThreadScheduler(ThreadFactory threadFactory) {
+		this.es = Executors.newSingleThreadExecutor(threadFactory);
+	}
 
-    @Override
-    public void destroy() {
-        es.shutdown();
-    }
+	@Override
+	public void schedule(Runnable task, Object actorId) {
+		if (!es.isShutdown()) {
+			es.execute(task);
+		}
+	}
+
+	@Override
+	public void destroy() {
+		es.shutdown();
+	}
 
 }
